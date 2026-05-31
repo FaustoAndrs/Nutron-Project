63bb2f46367e8dcb1e5c520dc84fef72931fab56
FaustoAndrs <79335408+FaustoAndrs@users.noreply.github.com>
2026-05-31 23:00:17 +0200

    añade modificaciones a servicios

--- nutron-app-kmp/composeApp/src/androidMain/kotlin/com/lazysyntax/nutron/data/remote/NetworkConfig.android.kt
+++ nutron-app-kmp/composeApp/src/androidMain/kotlin/com/lazysyntax/nutron/data/remote/NetworkConfig.android.kt
@@ -1,8 +1,7 @@
 package com.lazysyntax.nutron.data.remote
 
 actual val BASE_HOST: String
-    //get() = "localhost"
-    //get() = "10.0.2.2" //VM
-    //get() = "192.168.1.38" // En Windows con red wifi
-    //get() = "192.168.1.45" //En Mac con red Wifi
-    get() = "172.20.10.2" //Mac red iphone
+    // get() = "10.0.2.2" //VM
+    // get() = "192.168.1.38" //IP Windows en red wifi
+     get() = "192.168.1.45" //IP Mac con red wifi
+    // get() = "172.20.10.2" // IP Mac con punto de acceso IPhone
--- nutron-app-kmp/composeApp/src/commonMain/kotlin/com/lazysyntax/nutron/data/remote/NetworkConfig.kt
+++ nutron-app-kmp/composeApp/src/commonMain/kotlin/com/lazysyntax/nutron/data/remote/NetworkConfig.kt
@@ -3,11 +3,6 @@ package com.lazysyntax.nutron.data.remote
 expect val BASE_HOST: String
 
 object NetworkConstants {
-   /*
     val AUTH_BASE_URL = "http://$BASE_HOST:8081/api/v1"
     val DATA_BASE_URL = "http://$BASE_HOST:8082/api/v1"
-    */
-
-    val AUTH_BASE_URL = "http://auth.nutron.local:8081/api/v1"
-    val DATA_BASE_URL = "http://nutrition.nutron.local:8082/api/v1"
 }
