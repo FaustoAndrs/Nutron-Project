c19eccc66d8c15778e76d6b09808fbbf48b4abed
Fausto Andrés Chipantiza Carrasco <fauchicar@alu.edu.gva.es>
2026-05-31 22:11:37 +0200

    Añade dns config

--- nutron-app-kmp/composeApp/src/androidMain/kotlin/com/lazysyntax/nutron/data/remote/NetworkConfig.android.kt
+++ nutron-app-kmp/composeApp/src/androidMain/kotlin/com/lazysyntax/nutron/data/remote/NetworkConfig.android.kt
@@ -1,6 +1,8 @@
 package com.lazysyntax.nutron.data.remote
 
 actual val BASE_HOST: String
+    //get() = "localhost"
     //get() = "10.0.2.2" //VM
-    //get() = "192.168.1.38" //Windows
-      get() = "192.168.1.45" //Mac
+    //get() = "192.168.1.38" // En Windows con red wifi
+    //get() = "192.168.1.45" //En Mac con red Wifi
+    get() = "172.20.10.2" //Mac red iphone
--- nutron-app-kmp/composeApp/src/iosMain/kotlin/com/lazysyntax/nutron/data/remote/NetworkConfig.ios.kt
+++ nutron-app-kmp/composeApp/src/iosMain/kotlin/com/lazysyntax/nutron/data/remote/NetworkConfig.ios.kt
@@ -2,5 +2,6 @@ package com.lazysyntax.nutron.data.remote
 
 actual val BASE_HOST: String
     // get() = "localhost" //vm
-    // get() = "192.168.1.38" //Windows
-       get() = "192.168.1.45" //Mac
+    // get() = "192.168.1.38" //En Windows con red Wifi
+    // get() = "192.168.1.45" //En Mac con red Wifi
+    get() = "172.20.10.2" // En Mac con punto de acceso IPhone
